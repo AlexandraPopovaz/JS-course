5defc4d3546a3da6673238d9d69476b851cf8a35
Alexandra Popova <93454181+Mirimma@users.noreply.github.com>
2023-04-09 22:40:34 +0300

    added els for the root page

--- homework-5-master/functional/functional.js
+++ homework-5-master/functional/functional.js
@@ -54,29 +54,106 @@
             setItems([...items, `Item ${items.length + 1}`]);
         }
 
-        function deleteItem() {
-            setItems(items.slice(items.length - 2));
+        // function deleteItem() {
+        //     setItems(items.slice(items.length - 2));
+        // }
+
+        function openModal() {
+            modal.classList.remove("hidden");
         }
 
-        const div = document.createElement("div");
+        function closeModal() {
+            modal.classList.add("hidden");
+        }
+
+        const root = document.createElement("div");
+        root.classList.add("root");
+        const titleRoot = document.createElement("text");
+        titleRoot.textContent = "To Do List";
+        titleRoot.classList.add("titleRoot");
+
+        const wrapperInputButton = document.createElement("div");
+        wrapperInputButton.classList.add("wrapper");
+
+        const inputRoot = document.createElement("input");
+        inputRoot.classList.add("inputRoot");
+        inputRoot.placeholder = "Search Task";
+
+        const buttonNewTask = Button({ text: "+ New Task", onClick: openModal });
+        buttonNewTask.classList.add("buttonNewTask");
+
+        const allTasksSection = document.createElement("div");
+        allTasksSection.classList.add("allTasksSection");
+
+        const titleAllTasksSection = document.createElement("text");
+        titleAllTasksSection.textContent = "All Tasks";
+        titleAllTasksSection.classList.add("titleAllTasksSection");
+
+        const checkbox1 = document.createElement("input");
+        checkbox1.type = "checkbox";
+        checkbox1.name = "The very first task";
+
+        const checkbox2 = document.createElement("input");
+        checkbox2.type = "checkbox";
+        checkbox2.name = "The second task";
+
+        const checkbox3 = document.createElement("input");
+        checkbox3.type = "checkbox";
+        checkbox3.name = "3 task";
+
+        const checkbox4 = document.createElement("input");
+        checkbox4.type = "checkbox";
+        checkbox4.name = "4 task";
+
+        const completedTasksSection = document.createElement("div");
+        completedTasksSection.classList.add("completedTasksSection");
+        const titlecompletedTasksSection = document.createElement("text");
+        titlecompletedTasksSection.textContent = "Completed Tasks";
+        titlecompletedTasksSection.classList.add("titlecompletedTasksSection");
+
+        const checkbox5 = document.createElement("input");
+        checkbox5.type = "checkbox";
+        checkbox5.name = "5th task";
+
+        const checkbox6 = document.createElement("input");
+        checkbox6.type = "checkbox";
+        checkbox6.name = "6th task";
+
         const modal = document.createElement("div");
-        const input = document.createElement("input");
-        const title = "Add New Task";
-        input.placeholder = "Task Title";
+        modal.classList.add("modal", "hidden");
+
+        const inputModal = document.createElement("input");
+        const titleModal = "Add New Task";
+        inputModal.placeholder = "Task Title";
 
         const list = List({ items });
-        const button = Button({ text: "Add Task", onClick: addItem });
-        button.style.width = "227.5px";
-        button.style.height = "52px";
 
-        const buttonCancel = Button({ text: "Cancel", onClick: deleteItem });
-        buttonCancel.style.width = "227.5px";
-        buttonCancel.style.height = "52px";
-        modal.append(input);
+        const buttonAdd = Button({ text: "Add Task", onClick: addItem });
+        buttonAdd.style.width = "100px";
+        buttonAdd.style.height = "30px";
+        buttonAdd.style.borderRadius = "4%";
 
-        div.append(list, button, buttonCancel, modal, input, title);
+        const buttonCancel = Button({ text: "Cancel", onClick: closeModal });
+        buttonCancel.style.width = "100px";
+        buttonCancel.style.height = "30px";
 
-        return div;
+        // const buttonCancel = Button({ text: "Cancel", onClick: deleteItem });
+        // buttonCancel.style.width = "100px";
+        // buttonCancel.style.height = "30px";
+
+        completedTasksSection.append(titlecompletedTasksSection, checkbox5, checkbox6);
+
+        allTasksSection.append(titleAllTasksSection, checkbox1, checkbox2, checkbox3, checkbox4);
+
+        wrapperInputButton.append(inputRoot, buttonNewTask);
+
+        modal.append(titleModal, inputModal, buttonCancel, buttonAdd);
+        //modal.append(buttonsWrapper);
+        //mainWindow.append(modal);
+
+        root.append(titleRoot, modal, wrapperInputButton, allTasksSection, completedTasksSection);
+
+        return root;
     }
 
     /**
