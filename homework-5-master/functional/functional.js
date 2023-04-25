1cec5925eac1a6c24cb965b89d704bffbff8952a
Alexandra Popova <94701044+AlexandraPopovaz@users.noreply.github.com>
2023-04-25 20:53:42 +0300

    1.6 Homework 6 (#6)

--- homework-5-master/functional/functional.js
+++ homework-5-master/functional/functional.js
@@ -24,12 +24,14 @@
      * @returns {HTMLElement} - List element
      */
     function List({ items }) {
-        const listItems = items.map((item) => `<li><input type="checkbox">${item}</li>`).join("");
+        const listItems = items
+            .map((item) => `<li><input type="checkbox">${item.title}</li>`)
+            .join("");
         const ul = document.createElement("ul");
         ul.innerHTML = listItems;
 
-        const removeIcon = document.createElement("img");
-        removeIcon.classList.add("removeIcon");
+        // const removeIcon = document.createElement("img");
+        // removeIcon.classList.add("removeIcon");
 
         return ul;
     }
@@ -47,15 +49,56 @@
         return button;
     }
 
+    const SERVER_URL = "http://localhost:3004/tasks";
+
+    async function getTodos(query, endpoint = SERVER_URL) {
+        try {
+            query ? (query = `?${query}`) : (query = "");
+
+            const response = await fetch(`${endpoint}${query}`);
+
+            if (!response.ok) throw new Error(response.statusText);
+
+            const data = await response.json();
+            console.log(data);
+            return data;
+        } catch (err) {
+            console.error(err.message || err);
+        }
+    }
+
+    async function postTodo(data = {}) {
+        await fetch("http://localhost:3004/tasks", {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "application/json" },
+        });
+
+        return data;
+    }
+
     /**
      * App container
      * @returns {HTMLDivElement} - The app container
      */
+
     function App() {
-        const [items, setItems] = useState(["Task 1: complete HW5"]);
+        //const [items, setItems] = useState(["Task 1: complete HW5"]);
+        let items = [];
+        const allTasksSection = document.createElement("div");
+
+        function setItems(newItems) {
+            items = newItems;
+        }
 
         function addItem() {
-            setItems([...items, `${inputModal.value}`]);
+            const newItem = { id: Math.random(), title: `${inputModal.value}`, isCompleted: true };
+            postTodo(newItem);
+            setItems([...items, newItem]);
+
+            const listNew = List({ items });
+            allTasksSection.replaceChild(listNew, allTasksSection.childNodes[1]);
+            closeModal();
         }
 
         // function deleteItem() {
@@ -89,7 +132,6 @@
         const buttonNewTask = Button({ text: "+ New Task", onClick: openModal });
         buttonNewTask.classList.add("buttonNewTask");
 
-        const allTasksSection = document.createElement("div");
         allTasksSection.classList.add("allTasksSection");
 
         const titleAllTasksSection = document.createElement("text");
@@ -148,6 +190,12 @@
         buttonsWrapper.append(buttonCancel, buttonAdd);
 
         root.append(titleRoot, modal, wrapperInputButton, allTasksSection, completedTasksSection);
+
+        getTodos().then((data) => {
+            setItems(data);
+            const listNew = List({ items });
+            allTasksSection.replaceChild(listNew, allTasksSection.childNodes[1]);
+        });
 
         return root;
     }
