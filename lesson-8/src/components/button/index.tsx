4c8b8bd76eee7ba2242c595458680cf8699291bd
Alexandra Popova <93454181+Mirimma@users.noreply.github.com>
2023-05-01 20:09:14 +0300

    added react app with a component for buttons with usestate

---
+++ lesson-8/src/components/button/index.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./index.css";
+
+export const Button = () => {
+    const [counter, setCounter] = useState(0);
+
+    return (
+        <button className="App-button" onClick={() => setCounter(counter + 1)}>
+            I've pressed {counter} times
+        </button>
+    );
+};
